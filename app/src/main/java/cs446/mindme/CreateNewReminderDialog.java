51977539f11c7c59b792d84698ac3e230f3537de
Richard F <richardfa@v1020-wn-176-156.campus-dynamic.uwaterloo.ca>
2015-07-12 13:42:25 -0400

    createnewreminderdialog

--- app/src/main/java/cs446/mindme/CreateNewReminderDialog.java
+++ app/src/main/java/cs446/mindme/CreateNewReminderDialog.java
@@ -2,12 +2,19 @@ package cs446.mindme;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +32,7 @@ public class CreateNewReminderDialog extends Dialog implements
     public Activity c;
     public Dialog d;
     public Button cancel, send;
+    public String message;
     public TextView reminding;
 
     public MainActivity.Friend selectedFriend;
@@ -75,19 +83,70 @@ public class CreateNewReminderDialog extends Dialog implements
                     adapter.clearSelected();
                 }
                 updateReminding();
+                updateButton();
             }
         });
         cancel = (Button) findViewById(R.id.cancelnewreminder);
         send = (Button) findViewById(R.id.sendnewreminder);
         cancel.setOnClickListener(this);
         send.setOnClickListener(this);
+        final EditText editText = (EditText) findViewById(R.id.newReminderMessage);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.requestFocus();
+                editText.setFocusable(true);
+                editText.setFocusableInTouchMode(true);
+                editText.setSelected(true);
+                editText.setClickable(true);
+                Log.e("Edittext", editText.isEnabled() ? "Enabled" : "Disabled");
+                Log.e("Edittext", editText.isFocusable() ? "Focusable" : "Not Focusable");
+                Log.e("Edittext", editText.isFocused() ? "Focused" : "Not Focused");
+                Log.e("Edittext", editText.isInEditMode() ? "In Edit Mode" : "Not In Edit Mode");
+                Log.e("Edittext", editText.isSelected() ? "Selected" : "Unselected");
+            }
+        });
+        /*editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.e("test", "listened to change");
+                message = editText.getText().toString();
+                updateButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });*/
         updateReminding();
+        updateButton();
     }
 
     public void updateReminding() {
         reminding.setText("REMINDING: ");
         if (selectedFriend != null) {
             reminding.append(selectedFriend.name);
+        }
+    }
+
+    public void updateButton() {
+        if (selectedFriend == null) {
+            send.setEnabled(false);
+            return;
+        }
+        if (message == null || message.isEmpty()) {
+            if (send != null) {
+                send.setEnabled(false);
+            }
+        } else {
+            if (send != null) {
+                send.setEnabled(true);
+            }
         }
     }
 
